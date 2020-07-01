dc858a0776cf34d783dc4b8f52259777445b9962
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-07-01 20:09:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,13 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path==(/items/)
-      resp.write "#{@@item.last.price}"
+    if req.path==(/items/item_name)
+      item_name = req.params["q"]
+      if @@item.find(item_name)
+        resp.write "#{@@item.find(item_name).price}"
+      else
+        resp.write ""
+        resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 404
