b8e290195fe90312ed632756eb4e87e9c648e644
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-07-01 20:12:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       if item =@@item.find{|i| i.name == item_name}
         resp.write item.price
       else
-        resp.write ""
+        resp.write "Item not found"
         resp.status = 400
     else
       resp.write "Route not found"
